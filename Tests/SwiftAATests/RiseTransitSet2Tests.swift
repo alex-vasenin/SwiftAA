3ff65c8e6de13805f7681f927184057e4178c0af
Alexander Vasenin <alex.vasenin@me.com>
2024-07-14 14:50:27 -0500

    Add placeholder for `RiseTransitSet2Tests`

---
+++ Tests/SwiftAATests/RiseTransitSet2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  RiseTransitSet2.swift
+//  SwiftAATests
+//
+//  Created by Alexander Vasenin on 14/07/24.
+//  Copyright © 2024 onekiloparsec. All rights reserved.
+//
+
+import XCTest
+import SwiftAA
+
+class RiseTransitSet2Tests: XCTestCase {
+    func test() {
+        RiseTransitSet2.test()
+    }
+}
