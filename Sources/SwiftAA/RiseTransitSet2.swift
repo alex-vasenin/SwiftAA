4e9815af1500813d454bda66f721abaa6a39fee7
Alexander Vasenin <alex.vasenin@me.com>
2024-07-14 11:13:16 -0500

    Add placeholder for `RiseTransitSet2`

---
+++ Sources/SwiftAA/RiseTransitSet2.swift
@@ -0,0 +1,11 @@
+//
+//  RiseTransitSet2.swift
+//  SwiftAA
+//
+//  Created by Alexander Vasenin on 14/07/24.
+//  Copyright © 2024 onekiloparsec. All rights reserved.
+//
+
+import Foundation
+
+public struct RiseTransitSet2 {}
