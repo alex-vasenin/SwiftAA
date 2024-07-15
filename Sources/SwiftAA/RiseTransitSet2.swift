20071536aa5498cf933652a4f3418062f399414f
Alexander Vasenin <alex.vasenin@me.com>
2024-07-15 15:26:52 -0500

    Extract `RiseTransitSet2` extension

--- Sources/SwiftAA/RiseTransitSet2.swift
+++ Sources/SwiftAA/RiseTransitSet2.swift
@@ -11,35 +11,6 @@ import AABridge
 
 /// Implementation for the algorithms which obtain the Rise, Transit and Set times (revised version)
 public struct RiseTransitSet2 {
-    public typealias EventType = CAARiseTransitSetDetails2.`Type`
-    public typealias Object = CAARiseTransitSet2.Object
-    public typealias MoonAlgorithm = CAARiseTransitSet2.MoonAlgorithm
-    
-    typealias RawDetails = CAARiseTransitSetDetails2
-    
-    public struct Details {
-        /// The type of the event which has occurred
-        public var type: EventType
-        /// When the event occurred in TT
-        public var julianDay: JulianDay
-        /// Applicable for rise or sets only, this will be the bearing (degrees west of south) of the event
-        public var bearing: Degree?
-        /// For transits only, this will contain the geometric altitude in degrees of the center of the object not including correction for refraction
-        public var geometricAltitude: Degree?
-        /// For transits only, this will be true if the transit is visible
-        public var isAboveHorizon: Bool?
-        
-        init(rawValue: RawDetails) {
-            self.type = rawValue.type
-            self.julianDay = JulianDay(rawValue.JD)
-            let isRiseOrSet = type == .Rise || type == .Set
-            self.bearing = isRiseOrSet ? Degree(rawValue.Bearing) : nil
-            let isTransit = type == .NorthernTransit || type == .SouthernTransit
-            self.geometricAltitude = isTransit ? Degree(rawValue.GeometricAltitude) : nil
-            self.isAboveHorizon = isTransit ? rawValue.bAboveHorizon : nil
-        }
-    }
-    
     public static func calculate(object: Object,
                                  within dateInterval: JulianDayInterval,
                                  geographicCoordinates: GeographicCoordinates,
@@ -86,5 +57,36 @@ public struct RiseTransitSet2 {
                                                             h0.value,
                                                             stepInterval.inDays.value)
         return result.map { Details(rawValue: $0) }
+    }
+}
+
+extension RiseTransitSet2 {
+    public typealias EventType = CAARiseTransitSetDetails2.`Type`
+    public typealias Object = CAARiseTransitSet2.Object
+    public typealias MoonAlgorithm = CAARiseTransitSet2.MoonAlgorithm
+    
+    typealias RawDetails = CAARiseTransitSetDetails2
+    
+    public struct Details {
+        /// The type of the event which has occurred
+        public var type: EventType
+        /// When the event occurred in TT
+        public var julianDay: JulianDay
+        /// Applicable for rise or sets only, this will be the bearing (degrees west of south) of the event
+        public var bearing: Degree?
+        /// For transits only, this will contain the geometric altitude in degrees of the center of the object not including correction for refraction
+        public var geometricAltitude: Degree?
+        /// For transits only, this will be true if the transit is visible
+        public var isAboveHorizon: Bool?
+        
+        init(rawValue: RawDetails) {
+            self.type = rawValue.type
+            self.julianDay = JulianDay(rawValue.JD)
+            let isRiseOrSet = type == .Rise || type == .Set
+            self.bearing = isRiseOrSet ? Degree(rawValue.Bearing) : nil
+            let isTransit = type == .NorthernTransit || type == .SouthernTransit
+            self.geometricAltitude = isTransit ? Degree(rawValue.GeometricAltitude) : nil
+            self.isAboveHorizon = isTransit ? rawValue.bAboveHorizon : nil
+        }
     }
 }
